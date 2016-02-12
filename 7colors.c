80fedd549cd20ca88b25c9c073f21dac910277f1
robin <robin.tusseau@gmail.com>
2016-02-12 16:26:16 +0100

    fin tp

--- 7colors.c
+++ 7colors.c
@@ -5,10 +5,10 @@
 #include <stdio.h>  /* printf */
 #include <time.h>
 #include <stdlib.h>
-
+#include <string.h>
 /* We want a 30x30 board game by default */
 #define BOARD_SIZE 30 
-
+#define CELLS BOARD_SIZE*BOARD_SIZE
 /** Represent the actual current board game 
  * 
  * NOTE: global variables are usually discouraged (plus encapsulation in
@@ -16,7 +16,7 @@
  *  For this first assignment, no dinosaure will get you if you do that. 
  */
 char board[BOARD_SIZE * BOARD_SIZE] = { 0 }; // Filled with zeros
-
+int c1=1,c2=1;
 /** Retrieves the color of a given board cell. If out of bound return 0 */
 char get_cell(int x, int y) 
 {
@@ -80,13 +80,44 @@ void play(char color, char player)
 		{
 		  set_cell(i,j,player);
 		  t=1;
+		  if(player=='v')
+		    c1++;
+		  else
+		    c2++;
 		}
 	    }
 	}
     }
 }
-      
 
+int finish()
+{
+  if(c1>(BOARD_SIZE*BOARD_SIZE)/2)
+    return 1;
+  else if(c2>(BOARD_SIZE*BOARD_SIZE)/2)
+    return 2;
+  else
+    return 0;
+}
+
+
+void print_occupation()
+{
+  float f1=(float)c1/(float)CELLS;
+  printf("Joueur 1 : %f \n ",f1);
+  printf("Joueur 2 : %f \n ",((float)c2/(float)CELLS));
+}
+void turn(char player)
+{
+  char color_play;
+  printf("%c",player);
+  scanf("%c",&color_play);
+  play(color_play,player);
+  printf("\n");
+  getchar();
+  print_board();
+  print_occupation();
+}
 
 /** Program entry point */
 int main() 
@@ -97,15 +128,15 @@ int main()
    srand(time(NULL));
    init_board();
    print_board();
-   char color_play;
-   while (1)
+   while (42)
      {
-       printf("Joueur 1 ");
-       scanf("%c",&color_play);
-       play(color_play,'v');
-       printf("\n");
-       print_board();
-       
+       turn('v');
+       if(finish())
+	 break;
+       turn('^');
+       if(finish())
+	 break;
      }
+   printf("Joueur %d",finish());
    return 0; // Everything went well
 }
